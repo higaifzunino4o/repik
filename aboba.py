bdd2395c39511022d2372ade5b60a5fc1da9df57
jery <137625740+higaifzunino4o@users.noreply.github.com>
2023-09-29 00:00:13 +0300

    Add files via upload

---
+++ aboba.py
@@ -0,0 +1,14 @@
+from web3 import Web3
+
+# Подключение к ноде Эфириума
+w3 = Web3(Web3.HTTPProvider('https://eth.llamarpc.com'))
+
+# Адрес, для которого мы хотим узнать количество транзакций
+address = Web3.to_checksum_address('0xfcbe4138bf3591194b51b3856bc5742562ddf6ea')
+
+try:
+    # Получение количества транзакций
+    transaction_count = w3.eth.get_transaction_count(address)
+    print(f"Количество транзакций по адресу {address}: {transaction_count}")
+except Exception as e:
+    print(f"Ошибка при получении количества транзакций: {str(e)}")
